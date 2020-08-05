e1c6dd6db01b3720979eaefcccd58f5403886828
rajesh-zero <rajesh_zero@outlook.com>
2020-08-05 22:09:02 +0530

    loading animation

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -3,12 +3,16 @@ const form = document.forms['google-sheet']
 
 form.addEventListener('submit', e => {
     e.preventDefault()
+
+    document.getElementById("contactForm").setAttribute("style", "display: none;")
+    document.getElementById("loading").setAttribute("style", "display: block;")
+
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
         // .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
         .then(response => successfullySubmitted())
         .catch(error => console.error('Error!', error.message))
 })
 function successfullySubmitted() {
-    document.getElementById("contactForm").setAttribute("style", "display: none;")
+    document.getElementById("loading").setAttribute("style", "display: none;")
     document.getElementById("formSubmitted").setAttribute("style", "display: block;")
 }
